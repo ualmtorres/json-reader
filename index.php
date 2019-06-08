bf2559f7f9a8cf575b908880df18b9e6655c047d
Manuel Torres Gil <mtorres@ual.es>
2019-06-08 07:54:33 +0200

    commit inicial

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$url = 'http://json/index.php'; // path to your JSON file
+$url = 'json/index.php'; // path to your JSON file
 echo 'Llamando a ' . $url;
-/*
+
 $data = file_get_contents($url); // put the contents of the file into a variable
 $characters = json_decode($data); // decode the JSON feed
 echo "<h1>Hola " . $characters->nombre . "</h1>";
-*/
+
 ?>
