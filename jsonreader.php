feb05f54af902c711229a065b14e26a614c76687
Manuel Torres Gil <mtorres@ual.es>
2019-06-08 07:40:23 +0200

    commit inicial

--- jsonreader.php
+++ jsonreader.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'http://json-borrar.app.di.ual.es/index.php'; // path to your JSON file
+$url = 'http://json/index.php'; // path to your JSON file
 $data = file_get_contents($url); // put the contents of the file into a variable
 $characters = json_decode($data); // decode the JSON feed
 echo "<h1>Hola " . $characters->nombre . "</h1>";
